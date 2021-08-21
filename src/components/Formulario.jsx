86d8741e7d0c78e03e1a6c018f5bc4d9b9bcde1a
Axel <axelfelix123@hotmail.com>
2021-08-21 01:13:31 -0500

    State del formulario y mensaje de error

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,15 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Formulario = () => {
+  // State del Formulario
+  const [busqueda, setBusqueda] = useState({
+    ciudad: '',
+    pais: ''
+  });
+
+  const [error, setError] = useState(false);
+
+  // Extraer informacion de busqueda
+  const { ciudad, pais } = busqueda;
+
+  // Función que coloca los elementos del state
+  const handleInputChange = e => {
+    // Actualizar el state
+    setBusqueda({
+      ...busqueda,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  // Cuando se envie el formulario
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    // Validar
+    if (ciudad.trim() === '' || pais.trim() === '') {
+      setError(true);
+      return;
+    }
+
+    // Paso validación
+    setError(false);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
+      {error && (
+        <p className="red darken-4 error">Todos los campos son obligatorios</p>
+      )}
+
       <div className="input-field col s12">
-        <input type="text" name="ciudad" id="ciudad" />
+        <input
+          type="text"
+          name="ciudad"
+          id="ciudad"
+          value={ciudad}
+          onChange={handleInputChange}
+        />
         <label htmlFor="ciudad">Ciudad:</label>
       </div>
 
       <div className="input-field col s12">
-        <select name="name" id="pais">
+        <select name="pais" id="pais" value={pais} onChange={handleInputChange}>
           <option value="" disabled selected>
             --Seleccione un país--
           </option>
@@ -22,6 +66,15 @@ export const Formulario = () => {
           <option value="PE">Perú</option>
         </select>
         <label htmlFor="pais">País:</label>
+      </div>
+
+      <div className="input-field col s12">
+        <button
+          type="submit"
+          className="waves-effect waves-light btn-large btn-block yellow accent-4 col s12"
+        >
+          Buscar Clima
+        </button>
       </div>
     </form>
   );
